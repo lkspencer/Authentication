4036e6ba578f0922071c195e7524a61500c41163
JNNS <kirk.spencer.jnns@statefarm.com>
2015-06-17 15:15:12 -0700

    getting RecognizeFace to at least compile.

--- Authentication/RecognizeFace.cs
+++ Authentication/RecognizeFace.cs
@@ -16,22 +16,24 @@
 
     private bool _IsTrained = false;
     private string recognizerType;
+    private int Eigen_threshold = 80;
+    private string Eigen_label = "EigenMatch";
 
     //*
     public string Recognise(Image<Gray, Byte> Input_image, int Eigen_Thresh = -1) {
       FaceRecognizer recognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
       if (_IsTrained) {
-        FaceRecognizer.PredictionResult ER = recognizer.Predict(Input_image);
+        FaceRecognizer.PredictionResult predictionResult = recognizer.Predict(Input_image);
         //Only use the post threshold rule if we are using an Eigen Recognizer 
         //since Fisher and LBHP threshold set during the constructor will work correctly
         switch (recognizerType) {
           case ("EMGU.CV.EigenFaceRecognizer"):
-            if (Eigen_Distance > Eigen_threshold) return Eigen_label;
+            if (predictionResult.Distance /*Eigen_Distance*/ > Eigen_Thresh) return Eigen_label;
             else return "Unknown";
           case ("EMGU.CV.LBPHFaceRecognizer"):
           case ("EMGU.CV.FisherFaceRecognizer"):
           default:
-            return Eigen_label; //the threshold set in training controls unknowns
+            return Eigen_label;
         }
       }
       return "";
