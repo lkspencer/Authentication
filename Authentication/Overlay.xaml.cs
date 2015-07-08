b84eb77c17bad91687711ea028bfee24edfc7ae8
JNNS <kirk.spencer.jnns@statefarm.com>
2015-07-08 14:23:40 -0700

    Small refactoring changes.

--- Authentication/Overlay.xaml.cs
+++ Authentication/Overlay.xaml.cs
@@ -1,23 +1,15 @@
 ﻿namespace Trainer {
-  using DirectShowLib;
-  using Emgu.CV;
-  using Emgu.CV.Structure;
   using Microsoft.Kinect;
   using Microsoft.Kinect.Face;
   using System;
   using System.Collections.Generic;
   using System.ComponentModel;
-  using System.Drawing.Imaging;
-  using System.IO;
-  using System.Linq;
-  using System.Threading.Tasks;
   using System.Windows;
   using System.Windows.Controls;
   using System.Windows.Input;
   using System.Windows.Media;
   using System.Windows.Media.Imaging;
 
-  // EMGU documentation link for our reference: http://www.emgu.com/wiki/files/3.0.0-alpha/document/html/b72c032d-59ae-c36f-5e00-12f8d621dfb8.htm
   public partial class Overlay : Window, INotifyPropertyChanged {
     // MainWindow Variables
     private KinectSensor kinectSensor = null;
