bc42ca6364be84086c04f4f1fc704b3cf01bc990
Delvin Hall <dlvhall@gmail.com>
2015-07-14 17:51:28 -0500

    Cropping of bmp

--- Basic/SimpleAuth.xaml.cs
+++ Basic/SimpleAuth.xaml.cs
@@ -6,6 +6,7 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using System;
 using System.Drawing;
+using System.IO;
 
 namespace Basic
 {
@@ -39,15 +40,56 @@ namespace Basic
             {
                 if (frame != null)
                 {
-                    Photo.Source = ToBitmap(frame);
+                    Bitmap p = BitmapFromSource(ToBitmap(frame));
+                    Photo.Source = ToImageSource(frame);
                     imageForCV = ToImage(frame);
-                    rectArray = DetectFace.Detect(imageForCV,)
+                    //imageForCV.Save(@"data\face.png");
+                   
 
+                    Rectangle[] rectArray;
+                    //Mat matImage = new Mat(@"data\face.png", Emgu.CV.CvEnum.LoadImageType.Grayscale);
+                    rectArray = DetectFace.Detect(imageForCV.Mat, @"haarcascade\haarcascade_eye.xml", @"haarcascade\haarcascade_frontalface_default.xml");
+
+                    if (rectArray.Length > 0)
+                    {
+                        Rectangle r = rectArray[0];
+                        //var resizedbitmap1 = Bitmap.createBitmap(bmp, 0,0,yourwidth, yourheight);
+
+
+                        Bitmap croppedBitmap = p.Clone(r, p.PixelFormat);
+                        
+                        byte[] pixels = new byte[400 * 400 * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+                        int stride = 400 * format.BitsPerPixel / 8;
+                        var bms = BitmapSource.Create(400, 400, 96, 96, format, null, pixels, stride);
+
+                        Face.Source = bms;
+                    }
                 }
             }
         }
 
-        private ImageSource ToBitmap(ColorFrame frame)
+        private ImageSource ToImageSource(ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        private BitmapSource ToBitmap(ColorFrame frame)
         {
             int width = frame.FrameDescription.Width;
             int height = frame.FrameDescription.Height;
@@ -89,6 +131,17 @@ namespace Basic
             img.Bytes = pixels;
 
             return img;
+        }
+
+        public static Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap; using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream); bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
         }
     }
 }
