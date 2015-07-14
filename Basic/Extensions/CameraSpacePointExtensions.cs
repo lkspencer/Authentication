1e0b6afa06c15cd80ca8b19094f09362c9bd16fc
zcope <zac.coplan@gmail.com>
2015-07-14 13:30:48 -0700

    Added Basic Project to solution

---
+++ Basic/Extensions/CameraSpacePointExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.Kinect {
+  using MathNet.Spatial.Euclidean;
+  using MathNet.Spatial.Units;
+
+  public static class CameraSpacePointExtensions {
+    public static CameraSpacePoint RotateX(this CameraSpacePoint target, double theta) {
+      var vector = new Vector3D(target.X, target.Y, target.Z);
+      var rotated = vector.Rotate(UnitVector3D.XAxis, Angle.FromRadians(theta));
+      target.X = (float)rotated.X;
+      target.Y = (float)rotated.Y;
+      target.Z = (float)rotated.Z;
+      return target;
+    }
+    public static CameraSpacePoint RotateY(this CameraSpacePoint target, double theta) {
+      var vector = new Vector3D(target.X, target.Y, target.Z);
+      var rotated = vector.Rotate(UnitVector3D.YAxis, Angle.FromRadians(theta));
+      target.X = (float)rotated.X;
+      target.Y = (float)rotated.Y;
+      target.Z = (float)rotated.Z;
+      return target;
+    }
+    public static CameraSpacePoint RotateZ(this CameraSpacePoint target, double theta) {
+      var vector = new Vector3D(target.X, target.Y, target.Z);
+      var rotated = vector.Rotate(UnitVector3D.ZAxis, Angle.FromRadians(theta));
+      target.X = (float)rotated.X;
+      target.Y = (float)rotated.Y;
+      target.Z = (float)rotated.Z;
+      return target;
+    }
+
+    public static CameraSpacePoint Translate(this CameraSpacePoint point, float dx, float dy, float dz) {
+      return new CameraSpacePoint {
+        X = point.X + dx,
+        Y = point.Y + dy,
+        Z = point.Z + dz
+      };
+    }
+  }
+}
