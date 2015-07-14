1667e7d60e20f72888b6ab9e15b63e6a1ed41bd4
zcope <zac.coplan@gmail.com>
2015-07-14 12:13:04 -0700

    Committing Basic Auth

---
+++ BasicAuth/Basic.xaml.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace BasicAuth
+{
+    /// <summary>
+    /// Interaction logic for BasicAuth.xaml
+    /// </summary>
+    public partial class Basic : Window
+    {
+
+        /// <summary>
+        /// Drawing image that we will display
+        /// </summary>
+        private DrawingImage imageSource;
+
+
+        /// <summary>
+        /// Drawing group for body rendering output
+        /// </summary>
+        private DrawingGroup drawingGroup;
+
+        /// <summary>
+        /// List of brushes for each face tracked
+        /// </summary>
+        private List<Brush> faceBrush;
+
+        /// <summary>
+        /// Display rectangle
+        /// </summary>
+        private Rect displayRect;
+
+
+        /// <summary>
+        /// Thickness of face bounding box and face points
+        /// </summary>
+        private const double DrawFaceShapeThickness = 8;
+
+        /// <summary>
+        /// Width of display (color space)
+        /// </summary>
+        private int displayWidth;
+
+        /// <summary>
+        /// Height of display (color space)
+        /// </summary>
+        private int displayHeight;
+
+
+        KinectSensor _sensor = null;
+        ColorFrameReader _colorReader = null;
+        
+        BodyFrameReader _bodyReader = null;
+        IList<Body> _bodies = null;
+
+        // 1) Specify a face frame source and a face frame reader
+        FaceFrameSource _faceSource = null;
+        FaceFrameReader _faceReader = null;
+        public Basic()
+        {
+            InitializeComponent();
+
+            this.displayRect = new Rect(0.0, 0.0, this.displayWidth, this.displayHeight);
+
+            // populate face result colors - one for each face index
+            this.faceBrush = new List<Brush>()
+            {
+                Brushes.White, 
+                Brushes.Orange,
+                Brushes.Green,
+                Brushes.Red,
+                Brushes.LightBlue,
+                Brushes.Yellow
+            };
+
+
+
+            // Create an image source that we can use in our image control
+            this.imageSource = new DrawingImage(this.drawingGroup);
+
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+
+                _colorReader = _sensor.ColorFrameSource.OpenReader();                _colorReader.FrameArrived += ColorReader_FrameArrived;
+
+                _bodyReader = _sensor.BodyFrameSource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                // 2) Initialize the face source with the desired features
+                _faceSource = new FaceFrameSource(_sensor, 0, FaceFrameFeatures.BoundingBoxInColorSpace |
+                                                              FaceFrameFeatures.FaceEngagement |
+                                                              FaceFrameFeatures.Glasses |
+                                                              FaceFrameFeatures.Happy |
+                                                              FaceFrameFeatures.LeftEyeClosed |
+                                                              FaceFrameFeatures.MouthOpen |
+                                                              FaceFrameFeatures.PointsInColorSpace |
+                                                              FaceFrameFeatures.RightEyeClosed);
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+            }
+        }
+
+        void ColorReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    camera.Source = frame.ToBitmap();
+                }
+
+            }
+        }
+
+        void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    Body body = _bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            // 4) Assign a tracking ID to the face source
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        void FaceReader_FrameArrived(object sender, FaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    // 4) Get the face frame result
+                    FaceFrameResult result = frame.FaceFrameResult;
+
+                    if (result != null)
+                    {
+                        // 5) Do magic!
+
+                        // Get the face points, mapped in the color space.
+
+                        var eyeLeft = result.FacePointsInColorSpace[FacePointType.EyeLeft];
+                        var eyeRight = result.FacePointsInColorSpace[FacePointType.EyeRight];
+                        var nose = result.FacePointsInColorSpace[FacePointType.Nose];
+                        var mouthLeft = result.FacePointsInColorSpace[FacePointType.MouthCornerLeft];
+                        var mouthRight = result.FacePointsInColorSpace[FacePointType.MouthCornerRight];
+
+                        var eyeLeftClosed = result.FaceProperties[FaceProperty.LeftEyeClosed];
+                        var eyeRightClosed = result.FaceProperties[FaceProperty.RightEyeClosed];
+                        var mouthOpen = result.FaceProperties[FaceProperty.MouthOpen];
+
+
+
+
+                        // draw the face bounding box
+                        //var faceBoxSource = result.FaceBoundingBoxInColorSpace;
+                        //Rect faceBox = new Rect(faceBoxSource.Left, faceBoxSource.Top, faceBoxSource.Right - faceBoxSource.Left, faceBoxSource.Bottom - faceBoxSource.Top);
+                        //using (DrawingContext dc = this.drawingGroup.Open())
+                        //{
+                        //    Brush drawingBrush = this.faceBrush[0];
+                        //    Pen drawingPen = new Pen(drawingBrush, DrawFaceShapeThickness);
+                        //    dc.DrawRectangle(null, drawingPen, faceBox);
+                        //}
+
+
+
+
+
+
+
+
+
+       
+                    }
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_colorReader != null)
+            {
+                _colorReader.Dispose();
+                _colorReader = null;
+            }
+
+            if (_bodyReader != null)
+            {
+                _bodyReader.Dispose();
+                _bodyReader = null;
+            }
+
+            if (_faceReader != null)
+            {
+                _faceReader.Dispose();
+                _faceReader = null;
+            }
+
+            if (_faceSource != null)
+            {
+                _faceSource.Dispose();
+                _faceSource = null;
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
