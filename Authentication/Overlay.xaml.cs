63aab51a0239dd51c4f13c9d64eb4eab85fb8a0d
lkspencer <kirk@lkspencer.com>
2015-07-14 21:30:21 -0500

    Identified points of interest for 3D verification.

--- Authentication/Overlay.xaml.cs
+++ Authentication/Overlay.xaml.cs
@@ -389,6 +389,48 @@
 
 
 
+
+
+
+
+      // face points that we will use for 3D authentication
+      //      HighDetailFacePoints_EyeLeft = 0,
+      //HighDetailFacePoints_LefteyeInnercorner = 210,
+      //HighDetailFacePoints_LefteyeOutercorner = 469,
+      //----HighDetailFacePoints_LefteyeMidtop = 241,
+      //----HighDetailFacePoints_LefteyeMidbottom = 1104,
+      //HighDetailFacePoints_RighteyeInnercorner = 843,
+      //HighDetailFacePoints_RighteyeOutercorner = 1117,
+      //----HighDetailFacePoints_RighteyeMidtop = 731,
+      //----HighDetailFacePoints_RighteyeMidbottom = 1090,
+      //----HighDetailFacePoints_LefteyebrowInner = 346,
+      //HighDetailFacePoints_LefteyebrowOuter = 140,
+      //----HighDetailFacePoints_LefteyebrowCenter = 222,
+      //----HighDetailFacePoints_RighteyebrowInner = 803,
+      //HighDetailFacePoints_RighteyebrowOuter = 758,
+      //----HighDetailFacePoints_RighteyebrowCenter = 849,
+      //----HighDetailFacePoints_MouthLeftcorner = 91,
+      //----HighDetailFacePoints_MouthRightcorner = 687,
+      //----HighDetailFacePoints_MouthUpperlipMidtop = 19,
+      //----HighDetailFacePoints_MouthUpperlipMidbottom = 1072,
+      //----HighDetailFacePoints_MouthLowerlipMidtop = 10,
+      //----HighDetailFacePoints_MouthLowerlipMidbottom = 8,
+      //HighDetailFacePoints_NoseTip = 18,
+      //HighDetailFacePoints_NoseBottom = 14,
+      //HighDetailFacePoints_NoseBottomleft = 156,
+      //HighDetailFacePoints_NoseBottomright = 783,
+      //HighDetailFacePoints_NoseTop = 24,
+      //HighDetailFacePoints_NoseTopleft = 151,
+      //HighDetailFacePoints_NoseTopright = 772,
+      //----HighDetailFacePoints_ForeheadCenter = 28,
+      //----HighDetailFacePoints_LeftcheekCenter = 412,
+      //----HighDetailFacePoints_RightcheekCenter = 933,
+      //----HighDetailFacePoints_Leftcheekbone = 458,
+      //----HighDetailFacePoints_Rightcheekbone = 674,
+      //----HighDetailFacePoints_ChinCenter = 4,
+      //----HighDetailFacePoints_LowerjawLeftend = 1307,
+      //----HighDetailFacePoints_LowerjawRightend = 1327
+
       bool goodFrame = true;
       var p18 = this.kinectSensor.CoordinateMapper.MapCameraPointToDepthSpace(hdFaceVertices[18]);
       if (float.IsInfinity(p18.X) || float.IsInfinity(p18.Y)) {
